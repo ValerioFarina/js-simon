8e0e9f0449ee37a2e0b4c32cda00ea65d0c94124
Valerio Farina <valerio.farina.1994@gmail.com>
2020-10-29 15:13:29 +0100

    Mostro i 5 numeri in pagina e imposto un timer che dopo 30 secondi li rimuove dalla pagina

--- main.js
+++ main.js
@@ -4,22 +4,25 @@
               Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.  */
 
 $(document).ready(function() {
-    // predispongo un array vuoto
+    // predispondo un array vuoto
     var numbers = [];
     for (var i = 0; i < 5; i++) {
         // genero un numero casuale (compreso, per esempio, tra 1 e 100)
         var randomNumber = getRndInteger(1, 100);
         // salvo il numero generato nell'array numbers
-        numbers.push(randomNumber)
+        numbers.push(randomNumber);
+        // aggiungo al div con classe "numbers" un h1 contenente il numero generato in modo da visualizzare il numero in pagina
+        $('.numbers').append('<h1>' + randomNumber + '</h1>');
     }
-    // visualizzo tramite un alert la lista dei numeri generati
-    alert(numbers);
+    console.log(numbers);
+    // imposto un timer di 30 secondi
+    setTimeout(function() {
+        // cancello i numeri visualizzati in pagina
+        $('.numbers').empty();
+    }, 30000);
+
 
 });
-
-
-// visualizzo il numero generato tramite un alert
-// alert(randomNumber);
 
 
 
